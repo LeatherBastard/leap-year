1e5c8aed2027b7987d5335ba4a24b794d5958db7
LeatherBastard <kostrykinmark@yandex.ru>
2023-03-22 14:46:02 +0300

    Add functionality, working version

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(reader.readLine());
+        if (isLeapYear(year)) {
+            System.out.println(year);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
+    }
+}
